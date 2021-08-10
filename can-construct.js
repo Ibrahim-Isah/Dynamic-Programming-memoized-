5070a692754a3fa348abafac869b2a60bf919f53
Isah Ibrahim <isahibn08@gmail.com>
2021-08-10 17:07:13 +0100

    brute force testing

---
+++ can-construct.js
@@ -0,0 +1,20 @@
+// This function takes two params the target and wordBank(array of words) and will check if it is possible for the words from the wordBank to form the target
+
+const canConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === '') return true;
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank, memo) === true) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+  memo[target] = false;
+  return false;
+};
+
+console.log(canConstruct('Hello', ['Hi', 'el', 'H', 'o']));
