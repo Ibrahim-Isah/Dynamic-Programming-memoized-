2ba3444eb073bd53f4e6dd8bc099292f342dde37
Isah Ibrahim <isahibn08@gmail.com>
2021-08-10 15:23:55 +0100

    created the grid traveler

---
+++ grid-traveler.js
@@ -0,0 +1,2 @@
+// Say that you are a traveler on a 2D grid, you begin in the top left corner and your goal is to reach the bottom right corner. You may only move down or right
+// Find the total number of ways you can travel to the bottom right
